de45894d50acb5887a563d30db371966666fed6d
Hadi Khanian <hadi.khanian@gmail.com>
2023-06-14 17:50:16 +0330

    Refactor Make Invoice in Pasargad Payment

--- core/Bina_Woocommerce_Payment_Pasargad.php
+++ core/Bina_Woocommerce_Payment_Pasargad.php
@@ -2,6 +2,9 @@
 
 namespace Bina\WoocommercePayment\Core;
 
+use Shetabit\Multipay\Invoice;
+use Throwable;
+use WC_Order;
 use WC_Payment_Gateway;
 
 class Bina_Woocommerce_Payment_Pasargad extends WC_Payment_Gateway
@@ -51,5 +54,32 @@ class Bina_Woocommerce_Payment_Pasargad extends WC_Payment_Gateway
 		$pasargad_settings = get_option('woocommerce_bina_woocommerce_payment_pasargad_settings');
 		$pasargad_settings['certificate'] = $_POST['woocommerce_bina_woocommerce_payment_pasargad_certificate'] ?? null;
 		update_option('woocommerce_bina_woocommerce_payment_pasargad_settings', $pasargad_settings);
+	}
+
+	public function make_invoice(WC_Order $order): ?Invoice
+	{
+		try {
+			// Create New Invoice Object
+			$invoice = new Invoice;
+
+			// Set Invoice Amount.
+			if (get_woocommerce_currency() === 'IRR') {
+				$invoice->amount($order->get_total());
+			} else {
+				$invoice->amount($order->get_total() * 10);
+			}
+
+			// Set Invoice Details
+			$invoice->detail([
+				'orderId' => $order->get_id(),
+				'name'    => $order->get_user()->first_name . ' ' . $order->get_user()->last_name,
+				'mobile'  => $order->get_billing_phone() ?? $order->get_user()->user_login,
+				'email'   => $order->get_billing_email() ?? $order->get_user()->user_email,
+			]);
+
+			return $invoice;
+		} catch (Throwable $e) {
+			return null;
+		}
 	}
 }
