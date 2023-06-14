355089bd185b2726767dfa3e12f386a715bf4ff7
Hadi Khanian <hadi.khanian@gmail.com>
2023-06-14 19:08:34 +0330

    Refactor Invoice Verify Method in Pasargad

--- core/Bina_Woocommerce_Payment_Pasargad.php
+++ core/Bina_Woocommerce_Payment_Pasargad.php
@@ -3,6 +3,7 @@
 namespace Bina\WoocommercePayment\Core;
 
 use Shetabit\Multipay\Invoice;
+use Shetabit\Multipay\Payment;
 use Throwable;
 use WC_Order;
 use WC_Payment_Gateway;
@@ -80,6 +81,55 @@ class Bina_Woocommerce_Payment_Pasargad extends WC_Payment_Gateway
 			return $invoice;
 		} catch (Throwable $e) {
 			return null;
+		}
+	}
+
+	public function verify()
+	{
+		// Get Request
+		$order_id = absint($_REQUEST['wc_order']) ?? 0;
+
+		// Check Cancel Transaction
+		if (empty($order_id)) {
+			wc_add_notice(__('Order ID is Empty! System can`t find your order data.', 'bina-woocommerce-payment'), 'error');
+			wp_redirect(wc_get_checkout_url());
+			exit;
+		}
+
+		// Get Order
+		$order = new WC_Order($order_id);
+
+		// Check Order is Unpaid
+		if ($order->is_paid()) {
+			wp_redirect(wc_get_checkout_url());
+			exit;
+		}
+
+		// Verify Transaction
+		try {
+			$payment = new Payment($this->paymentConfig());
+			if (get_woocommerce_currency() === 'IRR') {
+				$receipt = $payment->amount($order->get_total())->verify();
+			} else {
+				$receipt = $payment->amount($order->get_total() * 10)->verify();
+			}
+
+			// Add Order Note
+			$note = sprintf(__('The transaction was successful. The tracking number is %s', 'bina-woocommerce-payment'), $receipt->getReferenceId());
+			$order->add_order_note($note, 1);
+
+			// Process Order Transaction
+			$order->payment_complete($receipt->getReferenceId());
+			$order->save();
+
+			// Redirect to Thank You Message
+			wc_add_notice(sprintf(__('The transaction was successful. The tracking number is %s', 'bina-woocommerce-payment'), $receipt->getReferenceId()));
+			wp_redirect(add_query_arg('wc_status', 'success', $this->get_return_url($order)));
+			exit;
+		} catch (Throwable $e) {
+			wc_add_notice($e->getMessage(), 'error');
+			wp_redirect(wc_get_checkout_url());
+			exit;
 		}
 	}
 }
